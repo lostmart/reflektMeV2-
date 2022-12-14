a51affec67dc32af5d68c167106ad46a35a320de
lostmart <lostmart@hotmail.com>
2022-12-14 12:27:56 +0100

    delete now

--- src/components/ModalFooter.vue
+++ src/components/ModalFooter.vue
@@ -4,7 +4,7 @@
 
 <template>
 	<footer class="flex items-center justify-center translate-y-[0.7rem]">
-		<span class="text-levisGray text-xs">powered by now</span>
+		<span class="text-levisGray text-xs">powered by</span>
 		<img :src="reflektMeIcon" alt="powered by refleKtMe" class="ml-2" />
 	</footer>
 </template>
